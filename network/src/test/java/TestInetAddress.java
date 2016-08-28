a89737d753bfc9167e5b629278b9c9f90195884d
Joe Skora <jskora@gmail.com>
2016-08-27 21:07:20 -0400

    InetAddress tests.

---
+++ network/src/test/java/TestInetAddress.java
@@ -0,0 +1,18 @@
+
+import org.junit.Test;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+import static org.junit.Assert.fail;
+
+public class TestInetAddress {
+
+    @Test(expected = UnknownHostException.class)
+    public void testBadIPAddress() throws UnknownHostException {
+        final String BAD_IP = "300.300.300.300";
+        final InetAddress address = InetAddress.getByName(BAD_IP);
+        System.out.println("Address = " + address);
+        fail("Should not reach here because of UnknownHostException");
+    }
+}
